b66131d86f0539899763899f8f603f47598319d5
archita2511 <61596103+archita2511@users.noreply.github.com>
2023-11-05 12:10:19 -0600

    Delete react/PropertySalesApplication directory

--- react/PropertySalesApplication/src/components/ValidateButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ValidateButton = () => {
-  return (
-    <button onClick={() => alert('Property Validated!')}>Validate</button>
-  );
-}
-
-export default ValidateButton;
