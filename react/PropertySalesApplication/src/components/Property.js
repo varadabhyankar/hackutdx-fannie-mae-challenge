080296a6d270e5c5bfba4eaea1e29e79bab4c56f
Archita Ekkirala <archita.ekkirala@gmail.com>
2023-11-05 12:02:08 -0600

    Removing test

---
+++ react/PropertySalesApplication/src/components/Property.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Property = () => {
+  return (
+    <div>
+      <p>Property Name: XYZ</p>
+      <p>Property Price: $100,000</p>
+    </div>
+  );
+}
+
+export default Property;
---
+++ react/PropertySalesApplication/src/components/ValidateButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ValidateButton = () => {
+  return (
+    <button onClick={() => alert('Property Validated!')}>Validate</button>
+  );
+}
+
+export default ValidateButton;
